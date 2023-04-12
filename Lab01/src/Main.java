d42fd55d4c36ea46f6dbce765ac0f4f7aa48a447
Marcelo250437 <127454681+Marcelo250437@users.noreply.github.com>
2023-04-11 22:04:43 -0300

    Delete Lab01 directory

--- Lab01/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-	public static void main(String[] args) {
-		Cliente teste = new Cliente("joao","123.456.789-12","01/01/2000",23 ,"rua x" );
-		
-				System.out.println(teste.getNome());
-	}
-}
