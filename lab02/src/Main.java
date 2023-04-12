7223c3e172ae47fb373f512ea45772fdbe624059
Marcelo250437 <127454681+Marcelo250437@users.noreply.github.com>
2023-04-11 22:05:31 -0300

    Add files via upload

---
+++ lab02/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		Cliente teste = new Cliente("joao","123.456.789-12","01/01/2000",23 ,"rua x" );
+		
+				System.out.println(teste.getNome());
+	}
+}
