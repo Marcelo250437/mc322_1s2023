27041be82a4bd7056c00179806d0ebbb11c30eb4
Marcelo Inácio Martins de Aguiar <m250437@dac.unicamp.br>
2023-05-30 22:25:29 -0300

    30/05 v2

--- Lab03/src/Main.java
+++ Lab03/src/Main.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import javax.xml.crypto.Data;
 public class Main {
@@ -57,91 +58,112 @@ public class Main {
 	}
 
 	public static void inputs(){ //menu para realizar algumas operações
-		Seguradora porto = new Seguradora("Porto seguro", "8002-8922", "porto@seguros.com","av.paulista,321 - São Paulo-SP");
-		int opcoes;
+		String sair="";
 		Scanner input = new Scanner(System.in);
-		System.out.print("O que deseja fazer?\n"+
-		"1.Cadastrar clientePF\n"+
-		"2.Cadastrar clientePJ\n"+
-		"3.Remover cliente\n"+
-		"4.listar cliente\n"+
-		"5.Visualizar sinistro\n"+
-		"6.Listar sinistro\n");
-		opcoes = input.nextInt();
-		switch (opcoes){
-			//executando as opções escolhidas
-			case 1:
-				String vazio = input.nextLine();
-				System.out.print("");
-				System.out.println("digite seu nome: ");
-				String nome2 = input.nextLine();
-				System.out.print("digite seu endereço: ");
-				String endereco = input.nextLine();
-				System.out.print("digite seu data de Licenca no formato DD/MM/YYYY: ");
-				String lisc1 = input.nextLine();
-				System.out.print("digite seu nível de educação: ");
-				String educacao = input.nextLine();
-				System.out.print("digite seu gênero: ");
-				String genero = input.nextLine();
-				System.out.print("digite sua classe econômica: ");
-				String classe = input.nextLine();
-				System.out.print("digite seu CPF: ");
-				String cpf = input.nextLine();
-				System.out.print("digite seu data de nascimento no formato DD/MM/YYYY: ");
-				String nasc1 = input.nextLine();
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-				LocalDate lisc = LocalDate.parse(lisc1, formatter);
-				LocalDate nasc = LocalDate.parse(nasc1, formatter);
-				porto.cadastrarClientes(new ClientePF(nome2, endereco,lisc, educacao, genero, classe, cpf, nasc));
-				System.out.println(porto.listarClientes("PF")); 
-			case 2:
-				String vazio2 = input.nextLine();
-				System.out.print("");
-				System.out.print("digite o nome da empresa: ");
-				String nome3 = input.nextLine();
-				System.out.print("digite o endereço da empresa: ");
-				String endereco2 = input.nextLine();
-				System.out.print("digite o CNPJ: ");
-				String cnpj = input.nextLine();
-				System.out.print("digite a data de fundação no formato DD/MM/YYYY: ");
-				String fund = input.nextLine();
-				DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("d/MM/yyyy");
-				LocalDate fund1 = LocalDate.parse(fund, formatter1);
-				porto.cadastrarClientes(new ClientePJ(nome3, endereco2, cnpj, fund1));
-				System.out.println(porto.listarClientes("PJ"));
-			case 3:
-				String vazio3 = input.nextLine();
-				System.out.print("");
-				System.out.print("digite o nome do cliente a ser removido: ");
-				String cliente = input.nextLine();
-				porto.removerCliente(cliente);
-				System.out.println(porto.listarClientes("PF"));
-				System.out.println(porto.listarClientes("PJ"));
-			case 4:
-				String vazio4 = input.nextLine();
-				System.out.println("");
-				System.out.print("Listar PF ou PJ: ");
-				String condicao = input.nextLine();
-				if (condicao.equals("PF")){
-					System.out.println(porto.listarClientes("PF"));
-				}
-				else if (condicao.equals("PJ")){
-					System.out.println(porto.listarClientes("PJ"));
-				}
-			case 5:
-				String vazio6 = input.nextLine();
-				System.out.println("");
-				System.out.print("digite o nome do cliente: ");
-				String cliente5 = input.nextLine();
-				porto.visualizarSinistro(cliente5);
-			case 6:
-				String vazio7 = input.nextLine();
-				System.out.println("");
-				porto.listarSinistros();
+		do {
+			try{
+				Seguradora porto = new Seguradora("Porto seguro", "8002-8922", "porto@seguros.com","av.paulista,321 - São Paulo-SP");
+				int opcoes;
+				
+				System.out.print("O que deseja fazer?\n"+
+				"1.Cadastrar clientePF\n"+
+				"2.Cadastrar clientePJ\n"+
+				"3.Remover cliente\n"+
+				"4.listar cliente\n"+
+				"5.Visualizar sinistro\n"+
+				"6.Listar sinistro\n");
+				opcoes = input.nextInt();
+			
+				input.nextLine();
+				switch (opcoes){
+					//executando as opções escolhidas
+					case 1:
+						System.out.print("digite seu nome: ");
+						String nome2 = input.nextLine();
+						System.out.print("\ndigite seu endereço: ");
+						String endereco = input.nextLine();
+						System.out.print("digite seu data de Licenca no formato DD/MM/YYYY: ");
+						String lisc1 = input.nextLine();
+						System.out.print("digite seu nível de educação: ");
+						String educacao = input.nextLine();
+						System.out.print("digite seu gênero: ");
+						String genero = input.nextLine();
+						System.out.print("digite sua classe econômica: ");
+						String classe = input.nextLine();
+						System.out.print("digite seu CPF: ");
+						String cpf = input.nextLine();
+						System.out.print("digite seu data de nascimento no formato DD/MM/YYYY: ");
+						String nasc1 = input.nextLine();
+						DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+						LocalDate lisc = LocalDate.parse(lisc1, formatter);
+						LocalDate nasc = LocalDate.parse(nasc1, formatter);
+						porto.cadastrarClientes(new ClientePF(nome2, endereco,lisc, educacao, genero, classe, cpf, nasc));
+						System.out.println(porto.listarClientes("PF")); 
+						break;
+					case 2:
+						System.out.print("digite o nome da empresa: ");
+						String nome3 = input.nextLine();
+						System.out.print("digite o endereço da empresa: ");
+						String endereco2 = input.nextLine();
+						System.out.print("digite o CNPJ: ");
+						String cnpj = input.nextLine();
+						System.out.print("digite a data de fundação no formato DD/MM/YYYY: ");
+						String fund = input.nextLine();
+						DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("d/MM/yyyy");
+						LocalDate fund1 = LocalDate.parse(fund, formatter1);
+						porto.cadastrarClientes(new ClientePJ(nome3, endereco2, cnpj, fund1));
+						System.out.println(porto.listarClientes("PJ"));
+						break;
+					case 3:
+						System.out.print("digite o nome do cliente a ser removido: ");
+						String cliente = input.nextLine();
+						porto.removerCliente(cliente);
+						System.out.println(porto.listarClientes("PF"));
+						System.out.println(porto.listarClientes("PJ"));
+						break;
+					case 4:
+						System.out.print("Listar PF ou PJ: ");
+						String condicao = input.nextLine();
+						if (condicao.equals("PF")){
+							System.out.println(porto.listarClientes("PF"));
+						}
+						else if (condicao.equals("PJ")){
+							System.out.println(porto.listarClientes("PJ"));
+						}
+						break;
+					case 5:
+						System.out.println(porto.visualizarSinistro("Marcelo Aguiar"));
+						System.out.print("digite o nome do cliente: ");
+						String cliente5 = input.nextLine();
+						for(Cliente k: porto.listaClientes){
+							if(k.nome.equals(cliente5)){
+								porto.visualizarSinistro(k.nome);
+							}
+						}
+						break;
+					case 6:
+						porto.listarSinistros();
+						break;
 
-		}
+					}
+				}
+			catch(InputMismatchException e){
+
+				System.out.println();
+				continue;
+			}
+
+			finally{
+				System.out.println("Deseja sair do programa? sim ou nao");
+				sair = input.nextLine();	
+
+			}
 		
 
+			
+	}while(!sair.equals("sim"));
+
+		
 
 
 	}
